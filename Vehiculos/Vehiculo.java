8d9b67782d434a9d9e6c4a0c631a09d64360affb
Christofer <chhernandezga@est.utn.ac.cr>
2024-08-08 14:53:01 -0600

    Arreglo de Vehiculo

--- Vehiculos/Vehiculo.java
+++ Vehiculos/Vehiculo.java
@@ -9,5 +9,58 @@ package Vehiculos;
  * @author chris
  */
 public class Vehiculo {
+    private String matricula;
+    private String modelo;
+    private String marca;
+    private int añoFabriacion;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getAñoFabriacion() {
+        return añoFabriacion;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setAñoFabriacion(int añoFabriacion) {
+        this.añoFabriacion = añoFabriacion;
+    }
+
+    public Vehiculo(String matricula, String modelo, String marca, int añoFabriacion) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.añoFabriacion = añoFabriacion;
+    }
+
+    public Vehiculo() {
+        this("","","",0);
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", modelo=" + modelo + ", marca=" + marca + ", a\u00f1oFabriacion=" + añoFabriacion + '}';
+    }
+    
     
 }
