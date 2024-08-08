ebd861e5833f09fa6baea4d86641ef2857a1f652
Keizel <pamelamarchena76@gmail.com>
2024-08-08 14:08:31 -0600

    Clase Clientes
    Se crea la clase clientes con sus respectivos atributos, métodos y constructores

---
+++ Clientes/Cliente.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clientes;
+
+/**
+ *
+ * @author pamel
+ */
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String telefono; 
+    private String direccion; 
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Cliente(int id, String nombre, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+    
+    public Cliente(){
+        this(0, "", "", "");
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+    }
+}
